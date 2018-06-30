f8f99b0aac067b7f6d74f0eee668b0fb894241f7
Julia <juliafsfalcao@hotmail.com>
2018-06-30 17:57:19 -0300

    modificações (já começou a dar tudo errado)

--- src/main.py
+++ src/main.py
@@ -10,10 +10,12 @@ import sys, os
 FRIENDS = 1
 GROUPS = 2
 
+# whether to print debug stuff or not
+DEBUG = False
+
 def main():
     # for testing
-    h_id = GROUPS
-    k = 10
+    h_id = FRIENDS
     
     # build graph
     start_time = time.time()
@@ -21,15 +23,16 @@ def main():
     elapsed_time = time.time() - start_time
     print("=> runtime (graph construction): %0.4fs" % float(elapsed_time))
 
-    """
+    
     # run random-restart hill climbing search
     print("\nRANDOM-RESTART HILL CLIMBING:")
     start_time = time.time()
-    solution = search(graph, h_id, k)
+    solution = search(graph, h_id)
     elapsed_time = time.time() - start_time
-    print("\nsolution: " + str(solution))
+    print("solution: " + str(solution))
     print("=> runtime (heuristic search): %0.3fs" % float(elapsed_time))
     
+    """
     # run exact depth-first search
     print("\nEXACT DEPTH-FIRST SEARCH:")
     start_time = time.time()
@@ -71,19 +74,19 @@ def search(graph, heuristic_id, k = 1):
     iterations = k * 5
     it = 0 # iteration counter
 
-    print("k = " + str(k))
+    if (DEBUG): print("k = " + str(k))
 
     # for i in range(iterations):
     while it < iterations or len(local_maxima) < k:
         initial_node = random_node(graph)
 
-        print("\n-- search iteration " + str(it+1))
-        print("initial node: " + str(initial_node))
+        if (DEBUG): print("\n-- search iteration " + str(it+1))
+        if (DEBUG): print("initial node: " + str(initial_node))
 
         local_max, value = hill_climbing(graph, initial_node, heuristic_id, local_maxima)
 
-        print("local max: " + str(local_max))
-        print("value: " + str(value))
+        if (DEBUG): print("local max: " + str(local_max))
+        if (DEBUG): print("value: " + str(value))
         local_maxima.add((local_max, value))
 
         it += 1
@@ -169,15 +172,19 @@ def dfs(graph, heuristic_id, excluded):
 def tabu_search(graph, heuristic_id, tabu_size = 5):
     tabu_list = []
     initial_solution = random_node(graph) # ?
+    print(f"initial solution: {initial_solution}")
+
     best_solution = initial_solution
     best_candidate = initial_solution
     tabu_list.append(initial_solution)
-    max_it = 100
+    max_it = 20
     it = 0
 
     while (it < max_it): # stopping condition?
+        if (DEBUG): print(f"iteration {it}")
         s_neighborhood = list(graph.neighbors[best_candidate]) # ? build neighborhood
         best_candidate = s_neighborhood[0]
+        if (DEBUG): print(f"best candidate: {best_candidate}")
 
         for candidate in s_neighborhood:
             candidate_value = heuristic_function(heuristic_id, graph, candidate)
@@ -185,22 +192,26 @@ def tabu_search(graph, heuristic_id, tabu_size = 5):
 
             if candidate not in tabu_list and candidate_value > best_candidate_value:
                 best_candidate = candidate
+                if (DEBUG): print(f"best candidate: {best_candidate}")
         
         best_candidate_value = heuristic_function(heuristic_id, graph, best_candidate)
         best_solution_value = heuristic_function(heuristic_id, graph, best_solution)
 
         if best_candidate_value > best_solution_value:
             best_solution = best_candidate
+        
+        if (DEBUG): print(f"best solution: {best_solution}")
 
         tabu_list.append(best_candidate)
 
         if len(tabu_list) > tabu_size:
             tabu_list.pop(0)
+        
+        if (DEBUG): print(f"tabu list: {tabu_list}")
 
         it += 1
 
-    return best_solution
-
+    return (best_solution, heuristic_function(heuristic_id, graph, best_solution))
 
 
 main()
